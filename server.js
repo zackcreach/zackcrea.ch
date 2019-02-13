ba4d9df1d6b01a53e8013c26a9c09fce0eea0769
Zack Creach <zackcreach@gmail.com>
2019-02-12 22:19:35 -0500

    Added cors to server.js.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const port = 1337;
 
+app.use(cors());
 app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.set("views", path.resolve(__dirname, "./src/views"));
