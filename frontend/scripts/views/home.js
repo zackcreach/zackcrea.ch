4b3aa82a32367cdb3ab7f931f3357f9cfdab6725
Zack Creach <zackcreach@gmail.com>
2019-09-16 00:19:44 -0400

    Updated date.

--- frontend/scripts/views/home.js
+++ frontend/scripts/views/home.js
@@ -2,7 +2,7 @@ import Home from "./home.svelte";
 import "../../styles/home.css";
 import "../components/background";
 
-const weddingDate = new Date("9/15/2019").getTime();
+const weddingDate = new Date("11/2/2019").getTime();
 const nowDate = Date.now();
 const timeDifference = nowDate - weddingDate;
 const status = timeDifference > 0 ? "engaged" : "married";
