50e12e63df1e6eee942ebc744004ed951f854e66
Zack Creach <zackcreach@gmail.com>
2021-03-28 12:03:35 -0400

    Site back up and running on Next.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,41 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/home.module.css";
+import "../components/background";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <div>
-          <h1>Hi, I'm Zack.</h1>
+        <div className={styles.home}>
+          <h1 className={styles.title}>Hi, I'm Zack.</h1>
 
-          <a href="https://github.com/zackcreach">
-            I build things on the Internet.{" "}
-          </a>
+          <h2>
+            <a href="https://github.com/zackcreach" className={styles.link}>
+              I build things on the Internet.{" "}
+            </a>
+          </h2>
 
-          <br />
+          <h2>
+            <a href="https://skylardenney.com" className={styles.link}>
+              I'm married to Skylar Denney.
+            </a>
+          </h2>
 
-          <a href="https://skylardenney.com">I'm married to Skylar Denney.</a>
-
-          <br />
-
-          <a href="https://www.flickr.com/photos/zackcreach">I shoot film.</a>
+          <h2>
+            <a
+              href="https://www.flickr.com/photos/zackcreach"
+              className={styles.link}
+            >
+              I shoot film.
+            </a>
+          </h2>
         </div>
       </main>
-    </div>
+    </>
   );
 }
